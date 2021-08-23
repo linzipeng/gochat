780ebbc8d15d157ee405f929b05e939e1e0d18a9
zeppelin <zeppelin@zego.im>
2021-08-23 23:24:05 +0800

    dist

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.c9712706345fa9d969a6db9f4f63771b.js"
+  "precache-manifest.854c6de0fb12bc4943c29cc78742f5c0.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "go-enjoy"});
